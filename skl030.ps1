1af977bc556350b992d4c804cb58dcb5417381c6
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2013-10-16 13:22:23 +0900

    update skl030.ps1

--- skl030.ps1
+++ skl030.ps1
@@ -13,29 +13,37 @@ Param(
 [string]$MyName = $MyInvocation.MyCommand.Name
 [int32]$rc = 0
 ###############################################################################
+# main
+###############################################################################
 function main {
-  try {
+  $excel = ""
+  #try {
     toLog("START")
     # 処理
     chkTxtPath($txtPath)
-    chkBookPath($bookPath)
-    $recs = Get-Content -Path $txtPath
+    chkBookPath([ref]$bookPath)
+    toConsole($txtPath)
+    toConsole($bookPath)
+    $recs = Get-Content -Path $txtPath                # txtPathファイルの読み込み→配列
     $excel = New-Object -ComObject Excel.Application  # comオブジェクトの作成
+    $excel.DisplayAlerts = $false
     $workbook = $excel.Workbooks.add(1)               # シート追加
     txtToWorkbook -ref_workbook ([ref]$workbook) -ref_recs ([ref]$recs)
-    $excel.DisplayAlerts = $false
     $workbook.SaveAs($bookPath)
     toLog("SUCCESS")
-  } catch [Exception] {
+  #} catch [Exception] {
     # エラー処理
-    toLog("ERROR:" + $_.Exception.Message)
-    exit $rc
-  } finally {
-    $excel.quit() 
+    #toLog("ERROR Message:" + $_.Exception.Message)
+    #exit $rc
+  #} finally {
+    if ($excel -ne "") {
+      $excel.quit() 
+    }
     toLog("DONE")
-  }
-  exit $rc
+  #}
 }
+###############################################################################
+# workbook作成
 ###############################################################################
 function txtToWorkbook {
   Param(
@@ -59,15 +67,22 @@ function txtToWorkbook {
       $sheet.Cells.Item($row+1, $col+1) = $fields[$col]
       if ($row -eq 0) {
         $sheet.cells.item($row+1,$col+1).font.bold = $true
-        $sheet.cells.item($row+1,$col+1).borders.LineStyle = $lineStyle::xlDashDot
+        $sheet.cells.item($row+1,$col+1).borders.LineStyle = $lineStyle::xlContinuous
+        $sheet.cells.item($row+1,$col+1).borders.ColorIndex = $colorIndex::xlColorIndexAutomatic
+        $sheet.cells.item($row+1,$col+1).borders.weight = $borderWeight::xlMedium
+      } else {
+        $sheet.cells.item($row+1,$col+1).font.bold = $false
+        $sheet.cells.item($row+1,$col+1).borders.LineStyle = $lineStyle::xlContinuous
         $sheet.cells.item($row+1,$col+1).borders.ColorIndex = $colorIndex::xlColorIndexAutomatic
         $sheet.cells.item($row+1,$col+1).borders.weight = $borderWeight::xlMedium
       }
     }    
   }
-  $range = $ref_sheet.Value.usedRange
+  $range = $sheet.usedRange
   $range.EntireColumn.AutoFit() | out-null
 }
+###############################################################################
+# txtPathのチェック
 ###############################################################################
 function chkTxtPath {
   Param([string]$txtPath)
@@ -84,15 +99,32 @@ function chkTxtPath {
   }
 }
 ###############################################################################
+# bookPathのチェック
+###############################################################################
 function chkBookPath {
-  Param([string]$bookPath)
-  if ($bookPath -ne "") {
-    toLog($bookPath)
+  Param([ref]$ref_bookPath)
+  if ($ref_bookPath.Value -ne "") {
+    if ($ref_bookPath.Value -match "^\.\\") {
+      $pwd = Get-Location
+      $ref_bookPath.Value = $ref_bookPath.Value -replace "^\.", $pwd.Path
+    }
+    toLog($ref_bookPath.Value)
   } else {
     $rc = 1
     throw "txtPath not set"
   }
 }
+###############################################################################
+# コンソール出力
+###############################################################################
+function toConsole {
+  Param([string]$msg = "")
+  $now = get-date -uFormat "%Y/%m/%dT%H:%M:%S"
+  $now + ":" + "[" + $PID + "]" + " " + $msg |
+    Add-Content -Path $script:$logPath
+}
+###############################################################################
+# ログ書き込み
 ###############################################################################
 function toLog {
   Param([string]$msg = "")
@@ -101,5 +133,8 @@ function toLog {
     Out-File $script:$logPath -encoding Default -append
 }
 ###############################################################################
+# 実行部分
+###############################################################################
 Set-PSDebug -strict
 main
+exit $rc
