ca44bfe750fcd2911e921a5894879b2dd3fa1b79
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2013-12-26 21:07:55 +0900

    get-progid.ps1

---
+++ get-progid.ps1
@@ -0,0 +1,3 @@
+# http://gallery.technet.microsoft.com/scriptcenter/ce929426-71fc-4740-b8f1-b78ba1ba3b15
+Get-WmiObject -Class Win32_ProgIDSpecification |  
+Format-Table -Property ProgID, description -AutoSize | more
