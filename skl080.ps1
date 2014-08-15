e2b465604104eb5dfdff89f3cdfd6aef7c73e780
Tatsuo Indou <indou.tsystem@gmail.com>
2014-08-15 19:10:55 +0900

    update skl080.ps1

--- skl080.ps1
+++ skl080.ps1
@@ -8,18 +8,18 @@
 
 Import-Module $Module
 
-#$message = "テスト開始"
-#$rc = insert_message -status 0 -facility "ps1" -priority 0 -message $message
-#if ($rc -ne 0) {
-#	Write-Host $message
-#	exit $rc
-#}
-#
-#$message = "テスト終了"
-#$rc = insert_message -status 0 -facility "ps1" -priority 0 -message $message
-#if ($rc -ne 0) {
-#	Write-Host $message
-#	exit $rc
-#}
+$message = "テスト開始"
+$rc = insert_message -status 0 -facility "ps1" -priority 0 -message $message
+if ($rc -ne 0) {
+	Write-Host $message
+  exit $rc
+}
+
+$message = "テスト終了"
+$rc = insert_message -status 0 -facility "ps1" -priority 0 -message $message
+if ($rc -ne 0) {
+	Write-Host $message
+	exit $rc
+}
 
 select_messages
