259b7d997051362855b77dd06e10bba8e3efb0cc
Tatsuo Indou <indou.tsystem@gmail.com>
2014-09-25 02:36:52 +0900

    update ctl-proc020.ps1

--- ctl-proc020.ps1
+++ ctl-proc020.ps1
@@ -1,29 +1,46 @@
 
-$pinfos = @()
-$ps = @()
-$commands = @()
-$arguments = @()
+$commands = 
+@(
+  @{
+    command = "sleep.exe";
+    arguments = "3";
+  },
+  @{
+    command = "busy.exe";
+    arguments = "90000";
+  }
+  @{
+    command = "busy.exe";
+    arguments = "40000";
+  }
+)
 
-$commands.add("sleep.exe")
-$arguments.add("3")
-$commands.add("busy.exe")
-$arguments.add("30000")
+$pinfo = @()
+$proc = @()
 
-
-$pinfo = New-Object System.Diagnostics.ProcessStartInfo
-$pinfo.FileName = "sleep.exe" # ~\utils‚Ìsleep.exe(bcc32‚É‚æ‚é)
-$pinfo.RedirectStandardError = $true
-$pinfo.RedirectStandardOutput = $true
-$pinfo.UseShellExecute = $false
-$pinfo.Arguments = "2"
-
-$p = New-Object System.Diagnostics.Process
-$p.StartInfo = $pinfo
-$p.Start() | Out-Null
-
-$p.WaitForExit()
-$p.ExitCode
-$p.StartTime
-$p.ExitTime
-$p.Cpu
-$p | get-member | out-string > member.txt
+for ($i = 0; $i -lt $commands.length; $i++) {
+  $pinfo += New-Object System.Diagnostics.ProcessStartInfo
+  $pinfo[$i].FileName = $commands[$i].command
+  $pinfo[$i].Arguments = $commands[$i].arguments
+  $pinfo[$i].RedirectStandardError = $true
+  $pinfo[$i].RedirectStandardOutput = $true
+  $pinfo[$i].UseShellExecute = $false
+  $proc += New-Object System.Diagnostics.Process
+  $proc[$i].StartInfo = $pinfo[$i]
+  $proc[$i].Start() | Out-Null
+}
+for ($i = 0; $i -lt $commands.length; $i++) {
+  ("wait " + $commands[$i].command + " " + $commands[$i].arguments)
+  $proc[$i].WaitForExit()
+  $msg = $commands[$i].command + " " + $commands[$i].arguments
+  $msg += " done. "
+  $msg += ($proc[$i].StartTime.toString("yyyy/MM/dd HH:mm:ss"))
+  $msg += "-"
+  $msg += ($proc[$i].ExitTime.toString("yyyy/MM/dd HH:mm:ss"))
+  $msg += (" status was " + $proc[$i].ExitCode)
+  $msg
+  $out = $proc[$i].StandardOutput.ReadtoEnd()
+  $out
+  $err = $proc[$i].StandardError.ReadtoEnd()
+  $err
+}
