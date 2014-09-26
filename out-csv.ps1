9f8de57734993cf47f4effb4298e4eb22e568fc8
Tatsuo Indou <indou.tsystem@gmail.com>
2014-09-27 01:44:45 +0900

    update out-csv.ps1

--- out-csv.ps1
+++ out-csv.ps1
@@ -1,6 +1,10 @@
+
 try {
   set-psdebug -strict # •Ï”‰Šú‰»‚Ì‹­§
   $content = get-content -Path ./out-csv.csv | ConvertFrom-CSV
+  #$content = import-csv -Path ./out-csv.csv -Encoding Default  # Power Shell Ver3ˆÈ~
+  $content.GetType()
+  $content | get-member
   $content
 } catch [exception] {
   write-error $Error[0]
@@ -8,6 +12,6 @@ try {
   write-error ("InvocationInfo.PositionMessage:" + $Error[0].InvocationInfo.PositionMessage)
   exit 1
 } finally {
-  set-psdebug -off
+  set-psdebug -off  # •Ï”‰Šú‰»‚Ì‹­§OFF
   exit 0
 }
