b560b0b6d506e443bbd8360f861e193f314f4d5e
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2015-07-12 18:24:26 +0900

    add ping.ps1

---
+++ ping.ps1
@@ -0,0 +1,47 @@
+
+$interface = "10"
+$bytes = 32
+
+function run_and_check {
+	Param($cmd, $opt, $match_string)
+	$proc_info = New-Object System.Diagnostics.ProcessStartInfo
+	$proc_info.FileName = $cmd
+	$proc_info.Arguments = $opt
+	$proc_info.RedirectStandardError = $true
+	$proc_info.RedirectStandardOutput = $true
+	$proc_info.UseShellExecute = $false
+
+	$proc = New-Object System.Diagnostics.Process
+	$proc.StartInfo = $proc_info
+	$proc.Start() | Out-Null
+	$out = ""
+	$err = ""
+	while ($true) {
+		$out += $proc.StandardOutput.ReadtoEnd()
+		$err += $proc.StandardError.ReadtoEnd()
+		if ($proc.HasExited) {
+			break
+		}
+		Start-Sleep -m 100
+	}
+	$rc = $proc.ExitCode
+	if ($rc -ne 0) {
+		$proc.Close()
+	  throw ([String]::Format('{0} {1} status {2}.', $cmd, $opt, $rc))
+	}
+	$result = $out | Select-String -Pattern $match_string -Encoding Default
+	if ($result -eq $null) {
+		$proc.Close()
+	  throw ([String]::Format('{0} {1} output unmatch {2}.', $cmd, $opt, $match_string))
+	} else {
+		"OK"
+		[String]::Format('DEBUG:[{0}]は、[{1}]にマッチ', $out, $match_string)
+	}
+	$proc.Close()
+}
+
+$packet = 2
+$opt =  [String]::Format("-n {0} -l 32 192.168.11.1", $packet)
+$match_string = [String]::Format("パケット数: 送信 = {0}、受信 = {0}、損失 = 0 \(0% の損失\)", $packet)
+run_and_check "ping" $opt $match_string
+exit 0
