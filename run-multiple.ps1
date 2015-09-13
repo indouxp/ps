4f5cc50e3231e93b39938f4d77df4f88977eb4c9
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2015-09-13 16:47:07 +0900

    update run-multiple.ps1

--- run-multiple.ps1
+++ run-multiple.ps1
@@ -9,49 +9,44 @@ trap {
 	break
 }
 ###############################################################################
-function main {
-  try {
-    $multiple_file_path
-    $results = @()
-    $ids = @()
-    get-content $multiple_file_path -encoding Default |
-      ForEach-Object {
-        if ($_ -match "^#") { return }
-        $fields = -split $_
-        $arg = ""
-        for ($i = 1; $i -lt $fields.length; $i++) {
-          if ($fields[$i] -ne $null) {
-            $args += $fields[$i]  # $args配列に追加
-          }
+Set-PSDebug -strict
+
+try {
+  $multiple_file_path
+  $results = @()
+  $ids = @()
+  get-content $multiple_file_path -encoding Default |
+    ForEach-Object {
+      if ($_ -match "^#") { return }
+      $fields = -split $_
+      $arg = ""
+      for ($i = 1; $i -lt $fields.length; $i++) {
+        if ($fields[$i] -ne $null) {
+          $args += $fields[$i]  # $args配列に追加
         }
-        # プロセスの開始
-        if ($args -ne $null) {
-          $results += Start-Process -FilePath $fields[0] -ArgumentList $args -PassThru
-        } else {
-          $results += Start-Process -FilePath $fields[0] -PassThru
-        }
-        $ids += $results.id # プロセスIDを、$ids配列に追加
       }
-    # $ids配列をすべて処理して、wait(該当のPIDがない場合は無視)
-    $value =0
-    foreach ($id in $ids) {
-      try {
-        wait-process $id
-      } catch [Exception] {
-        if ($error[0].CategoryInfo.Category.toString() -ne "ObjectNotFound" -or
-            $error[0].CategoryInfo.Activity.toString() -ne "Wait-Process") {
-          throw $error[0]
-        }
+      # プロセスの開始
+      if ($args -ne $null) {
+        $results += Start-Process -FilePath $fields[0] -ArgumentList $args -PassThru
+      } else {
+        $results += Start-Process -FilePath $fields[0] -PassThru
+      }
+      $ids += $results.id # プロセスIDを、$ids配列に追加
+    }
+  # $ids配列をすべて処理して、wait(該当のPIDがない場合は無視)
+  $value =0
+  foreach ($id in $ids) {
+    try {
+      wait-process $id
+    } catch [Exception] {
+      if ($error[0].CategoryInfo.Category.toString() -ne "ObjectNotFound" -or
+          $error[0].CategoryInfo.Activity.toString() -ne "Wait-Process") {
+        throw $error[0]
       }
     }
-  } catch [Exception] {
-    $error[0]
-    $rc = 9
   }
-  $rc = 0
+} catch [Exception] {
+  $error[0]
+  exit 9
 }
-###############################################################################
-Set-PSDebug -strict
-$rc = 0
-main
-exit $rc
+exit 0
