53641c7388af6e9febcc34249afbc3099274858e
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2015-10-28 01:41:14 +0900

    add process2html.ps1

---
+++ process2html.ps1
@@ -0,0 +1,11 @@
+$a = "<style>"
+$a = $a + "BODY{background-color:peachpuff;}"
+$a = $a + "TABLE{border-width: 1px;border-style: solid;border-color: black;border-collapse: collapse;}"
+$a = $a + "TH{border-width: 1px;padding: 0px;border-style: solid;border-color: black;background-color:thistle}"
+$a = $a + "TD{border-width: 1px;padding: 0px;border-style: solid;border-color: black;background-color:PaleGoldenrod}"
+$a = $a + "</style>"
+
+$b = "<H1>services</H1>"
+
+#Get-Process | Select-Object Status, Name, DisplayName
+Get-Process | ConvertTo-HTML -head $a -body $b > ts.html
