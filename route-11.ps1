6a07c3cb40cfd3c4dd57ae315c84d46fe01c80aa
Tatsuo Indou <indou.tsystem@gmail.com>
2015-11-23 18:27:09 +0900

    update route-11.ps1

--- route-11.ps1
+++ route-11.ps1
@@ -7,23 +7,30 @@ param($if)
 $ErrorActionPreference = "Stop"
 set-variable -name script -value (get-item $MyInvocation.MyCommand.path) -option constant
 
+###############################################################################
 function add-route {
   param($ip, $gw)
   [String]::Format('add-route {0} {1}', $ip, $gw)
   &route add $ip mask 255.255.255.0 $gw if $if
+  return $lastexitcode
 }
 
+###############################################################################
 function delete-route {
   param($ip)
   [String]::Format('delete-route {0}', $ip)
   &route delete $ip
+  return $lastexitcode
 }
 
+###############################################################################
 function print-route {
   [String]::Format('print-route') 
   &route print
+  return $lastexitcode
 }
 
+###############################################################################
 function ping2 {
   param($ip)
   [String]::Format('ping {0}', $ip)
@@ -31,6 +38,7 @@ function ping2 {
   return $lastexitcode
 }
 
+###############################################################################
 function main {
   try {
     if ($if -eq $null) {
@@ -39,6 +47,10 @@ function main {
     [String]::Format('{0}:LastWriteTime:{1}', $script.name, $script.lastwritetime)
     for ($i = 1; $i -lt 256; $i++) {
       add-route "192.168.11.0" ([String]::Format('{0}.{1}', '192.168.0', $i))
+      $rc = $LastExitCode
+      if ($rc -ne 0) {
+        throw "add-route"
+      }
       ping2 "192.168.11.1"
       $rc = $LastExitCode
       [String]::Format('ping status {0}', $rc)
@@ -46,6 +58,10 @@ function main {
         break
       }
       delete-route "192.168.11.0"
+      $rc = $LastExitCode
+      if ($rc -ne 0) {
+        throw "delete-route"
+      }
     }
     print-route
   } catch [Exception] {
@@ -54,6 +70,7 @@ function main {
   }
 }
 
+###############################################################################
 Set-PSDebug -strict
 main | out-file -encoding default ($script.name + ".log")
 exit 0
